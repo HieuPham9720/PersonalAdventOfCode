80d4c1877f69c9f1c53c15f911b1de3b3851a547
HieuPham9720 <hieupham@hieus-mbp-2.mynetworksettings.com>
2023-12-06 18:55:46 -0500

    2023 day 6 part 2

--- 2023/day6.py
+++ 2023/day6.py
@@ -1,26 +1,9 @@
-result = 1
-
 with open("day6_input.txt", "r") as file:
-    times_line = file.readline()
-    # oneliner just for fun this time :)
-    times = " ".join(times_line.strip().split()).split(": ")[1].split(" ")
-    distances_line = file.readline()
-    distances = " ".join(distances_line.strip().split()).split(": ")[1].split(" ")
-    
-    for time, distance in zip(times, distances):
-        time = int(time)
-        distance = int(distance)
-        ways_to_win = 0
-        greater = False
-        for i in range(1, time + 1):
-            if i * (time - i) > distance:
-                greater = True
-                ways_to_win += 1
-            elif greater:
-                break
-        result *= ways_to_win
-        print(ways_to_win)
+    ways = 0
+    time = int(file.readline())
+    distance = int(file.readline())
+    for i in range(1, time + 1):
+        if i * (time - i) > distance:
+            ways += 1
 
-print(result)
-        
-
+print(ways)
