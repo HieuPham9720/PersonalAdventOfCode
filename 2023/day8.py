815841b94a8aac50b0effd009024347356434f0b
HieuPham9720 <hieupham@hieus-mbp-2.mynetworksettings.com>
2023-12-08 12:11:50 -0500

    2023 day 8 part 2

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,8 +1,9 @@
+import math
+
 # Map an island to it's L and R location
 # Example entry: {"LCP": ["FQJ", "JGH"]}
 mapping = {}
 steps = ""
-total_count = 0
 
 with open("day8_input.txt", "r") as file:
     for index, line in enumerate(file):
@@ -14,13 +15,27 @@ with open("day8_input.txt", "r") as file:
             # Since all data is in the form XXX = (XXX, XXX)
             mapping[line[:3]] = [line[7:10], line[12:15]]
 
-current = "AAA"
+node_moves = []
+for node in mapping.keys():
+    if node[2] == "A":
+        step_count = 0
+        while node[2] != "Z":
+            for step in steps:
+                node = mapping[node][0] if step == "L" else mapping[node][1]
+                step_count += 1
+                if node[2] == "Z":
+                    break
+        node_moves.append(step_count)
 
-while current != "ZZZ":
-    for step in steps:
-        current = mapping[current][0] if step == "L" else mapping[current][1]
-        total_count += 1
-        if current == "ZZZ":
-            break
+result = 1
+for move in node_moves:
+    result = math.lcm(result, move)
+print(result)
 
-print(total_count)
+
+
+"""
+Alternate solution:
+- For each node, find out how long it takes to reach one that ends in "Z"
+- Find the least common multple of those
+"""
