2252b91018ddd7232f26bfdb0184d3d147d3ed87
HieuPham9720 <hieupham@Hieus-MacBook-Pro-2.local>
2023-12-09 11:38:36 -0500

    2023 day 9 part 2

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,11 +1,13 @@
+from collections import deque
+
 result = 0
 
 with open("day9_input.txt", "r") as file:
     for line in file:
-        start_sequence = [int(x) for x in line.strip().split(" ")]
+        start_sequence = deque([int(x) for x in line.strip().split(" ")])
         sequences = [start_sequence]
         while not all(val == 0 for val in start_sequence):
-            new_sequence = []
+            new_sequence = deque()
             for i in range(len(start_sequence) - 1):
                 new_sequence.append(start_sequence[i + 1] - start_sequence[i])
             start_sequence = new_sequence
@@ -13,8 +15,8 @@ with open("day9_input.txt", "r") as file:
 
         # Go through all the past sequences to find new prediction
         for j in range(len(sequences) - 1, 0, -1):
-            sequences[j - 1].append(sequences[j - 1][-1] + sequences[j][-1])
+            sequences[j - 1].appendleft(sequences[j - 1][0] - sequences[j][0])
         
-        result += sequences[0][-1]
+        result += sequences[0][0]
 
 print(result)
