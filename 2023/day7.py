2aaaea51d3b33d5f45371cb19c42e0372b26a5d5
Hieu Pham <pmhbill@gmail.com>
2023-12-16 03:23:37 -0500

    2023 day 7 part 2

--- 2023/day7.py
+++ 2023/day7.py
@@ -1,8 +1,9 @@
 total = 0
 
+# Map to letter value for string comparison purpose ('A' > '9')
 convert_map = {
+    "J": "1",
     "T": "A",
-    "J": "B",
     "Q": "C",
     "K": "D",
     "A": "E",
@@ -45,23 +46,47 @@ with open("day7_input.txt", "r") as input_file:
         card_map = {}
         for card in hand:
             card_map[card] = card_map.get(card, 0) + 1
+    
+        joker_count = card_map.get("J", 0)
+        hand_type = ""
+
+        # For the comments, the first number is the joker_count
+        match len(card_map.values()):
+            case 1:
+                hand_type = "five_of_a_kind"
+            case 2:
+                match joker_count:
+                    case 0:
+                        if max(card_map.values()) == 4:     # 0 + 4/1
+                            hand_type = "four_of_a_kind"
+                        else:                               # 0 + 3/2
+                            hand_type = "full_house"
+                    case _:                                 
+                        hand_type = "five_of_a_kind"        
+            case 3:
+                match joker_count:
+                    case 0:
+                        if max(card_map.values()) == 3:     # 0 + 3/1/1
+                            hand_type = "three_of_a_kind"
+                        else:                               # 0 + 2/2/1
+                            hand_type = "two_pairs"
+                    case 1:
+                        if max(card_map.values()) == 3:     # 1 + 3/1
+                            hand_type = "four_of_a_kind"
+                        else:                               # 1 + 2/2
+                            hand_type = "full_house"
+                    case _:
+                        hand_type = "four_of_a_kind"        # 3 + 1/1 or 2 + 2/1
+            case 4:
+                match joker_count:
+                    case 0:                                 # 0 + 2/1/1/1
+                        hand_type = "one_pair"
+                    case _:                                 # 1 + 2/1/1 or 2 + 1/1/1
+                        hand_type = "three_of_a_kind"
+            case 5:
+                hand_type = "one_pair" if joker_count == 1 else "high_card"
         
-        if len(card_map.values()) == 1:
-            hand_types["five_of_a_kind"].append(hand)
-        elif len(card_map.values()) == 2:
-            if max(card_map.values()) == 4:
-                hand_types["four_of_a_kind"].append(hand)
-            else:
-                hand_types["full_house"].append(hand)
-        elif len(card_map.values()) == 3:
-            if max(card_map.values()) == 3:
-                hand_types["three_of_a_kind"].append(hand)
-            else:
-                hand_types["two_pairs"].append(hand)
-        elif len(card_map.values()) == 4:
-            hand_types["one_pair"].append(hand)
-        else:
-            hand_types["high_card"].append(hand)
+        hand_types[hand_type].append(hand)
 
      # Sort the hand for each hand type   
     for hand_type_list in hand_types:
