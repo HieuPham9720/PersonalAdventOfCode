881f99b305632b6cc0cfd4836a30e251aa2a6a59
Hieu Pham <pmhbill@gmail.com>
2023-12-16 16:15:53 -0500

    2023 day 13 attempt

--- 2023/day13.py
+++ 2023/day13.py
@@ -14,6 +14,14 @@ with open("day13_input.txt", "r") as input_file:
 result = 0
 
 for pattern in patterns:
+    largest_horizontal_pattern_size = 0
+    largest_vertical_pattern_size = 0
+
+    # Index of the row exactly above the line
+    horizontal_line_index = 0
+    # Index of the column exactly to the left of the line
+    vertical_line_index = 0
+
     # Horizontal line search:
     found = False
     start = pattern[0]
@@ -21,34 +29,55 @@ for pattern in patterns:
     while not found and r > 0:
         if pattern[r] == start:
             found = True
-            result += (1 + (r + 1)) // 2 * 100
+            if largest_horizontal_pattern_size < r + 1:
+                largest_horizontal_pattern_size = r + 1
+                horizontal_line_index = (r + 1) // 2
         r -= 1
     
-    l = 0
+    found = False
     start = pattern[-1]
+    l = 0
     while not found and l < len(pattern) - 1:
         if pattern[l] == start:
             found = True
-            result += ((l + 1) + len(pattern)) // 2 * 100
+            if largest_horizontal_pattern_size < len(pattern) - l: 
+                # Specifically: len(pattern) - 1 - l + 1
+                largest_horizontal_pattern_size = len(pattern) - l
+                horizontal_line_index = (len(pattern) - l) // 2
         l += 1
         
     # Vertical line search by flipping the pattern sideway
     pattern = ["".join(s) for s in zip(*pattern)]
+
+    found = False
     start = pattern[0]
     r = len(pattern) - 1
     while not found and r > 0:
         if pattern[r] == start:
             found = True
-            result += (1 + (r + 1)) // 2
+            if largest_vertical_pattern_size < r + 1:
+                largest_vertical_pattern_size = r + 1
+                vertical_line_index = (r + 1) // 2
         r -= 1
     
-    l = 0
+    found = False
     start = pattern[-1]
+    l = 0
     while not found and l < len(pattern) - 1:
         if pattern[l] == start:
             found = True
-            result += ((l + 1) + len(pattern)) // 2
+            if largest_vertical_pattern_size < len(pattern) - l: 
+                largest_vertical_pattern_size = len(pattern) - l
+                vertical_line_index = (len(pattern) - l) // 2
         l += 1
+
+    # Logic on what to add to result
+    if largest_horizontal_pattern_size > largest_vertical_pattern_size:
+        # Reflip to horizontal
+        pattern = ["".join(s) for s in zip(*pattern)]
+        result += (len(pattern) - horizontal_line_index) * 100
+    else:
+        result += len(pattern) - vertical_line_index
 
 print(result)
 
